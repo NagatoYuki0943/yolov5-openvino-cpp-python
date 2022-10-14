7cda97f282bc632f999b8787494d6934997925ac
yuki0943 <lihongtu12138@outlook.com>
2022-10-14 16:17:41 +0800

    添加yaml读取

--- python/main.py
+++ python/main.py
@@ -12,40 +12,17 @@ from openvino.runtime import Layout, Type
 import numpy as np
 import cv2
 
+from utils import resize_and_pad, post, get_index2label
+
 import time
 
 import os, sys
 os.chdir(sys.path[0])
 
 
+CONFIDENCE_THRESHOLD = 0.4
 SCORE_THRESHOLD = 0.2
 NMS_THRESHOLD = 0.4
-CONFIDENCE_THRESHOLD = 0.4
-
-
-def resize_and_pad(image, new_shape):
-    """缩放图片并填充为正方形
-
-    Args:
-        image (np.Array): 图片
-        new_shape (Tuple): [h, w]
-
-    Returns:
-        Tuple: 缩放的图片, 填充的宽, 填充的高
-    """
-    old_size = image.shape[:2]
-    ratio = float(new_shape[-1]/max(old_size)) #fix to accept also rectangular images
-    new_size = tuple([int(x*ratio) for x in old_size])
-    # 缩放高宽的长边为640
-    image = cv2.resize(image, (new_size[1], new_size[0]))
-    # 查看高宽距离640的长度
-    delta_w = new_shape[1] - new_size[1]
-    delta_h = new_shape[0] - new_size[0]
-    # 使用灰色填充到640*640的形状
-    color = [100, 100, 100]
-    img_reized = cv2.copyMakeBorder(image, 0, delta_h, 0, delta_w, cv2.BORDER_CONSTANT, value=color)
-
-    return img_reized, delta_w ,delta_h
 
 
 def get_image(image_path):
@@ -106,71 +83,11 @@ def get_model(model_path, device='CPU'):
     return infer_request
 
 
-def post(detections, delta_w ,delta_h, img):
-    """后处理
-
-    Args:
-        detections (np.Array): 检测到的数据 [25200, 85]
-        delta_w (int):  填充的宽
-        delta_h (int):  填充的高
-        img (np.Array): 原图
-
-    Returns:
-        np.Array: 绘制好的图片
-    """
-    boxes = []
-    class_ids = []
-    confidences = []
-    for prediction in detections:
-        confidence = prediction[4].item()
-        if confidence >= CONFIDENCE_THRESHOLD:
-            classes_scores = prediction[5:]
-            _, _, _, max_indx = cv2.minMaxLoc(classes_scores)
-            class_id = max_indx[1]
-            if (classes_scores[class_id] > .25):
-                confidences.append(confidence)
-                class_ids.append(class_id)
-                # 不是0~1之间的数据
-                x, y, w, h = prediction[0].item(), prediction[1].item(), prediction[2].item(), prediction[3].item()
-                xmin = x - (w / 2)
-                ymin = y - (h / 2)
-                box = np.array([xmin, ymin, w, h])
-                boxes.append(box)
-
-    indexes = cv2.dnn.NMSBoxes(boxes, confidences, SCORE_THRESHOLD, NMS_THRESHOLD)
-
-    detections = []
-    for i in indexes:
-        j = i.item()
-        detections.append({"class_index": class_ids[j], "confidence": confidences[j], "box": boxes[j]})
-
-    # Step 9. Print results and save Figure with detections
-    for detection in detections:
-
-        box = detection["box"]
-        classId = detection["class_index"]
-        confidence = detection["confidence"]
-        print( f"Bbox {i} Class: {classId} Confidence: {confidence}, Scaled coords: [ cx: {(box[0] + (box[2] / 2)) / img.shape[1]}, cy: {(box[1] + (box[3] / 2)) / img.shape[0]}, w: {box[2]/ img.shape[1]}, h: {box[3] / img.shape[0]} ]" )
-
-        # 还原到原图尺寸
-        box[0] = box[0] / ((640-delta_w) / img.shape[1])
-        box[2] = box[2] / ((640-delta_w) / img.shape[1])
-        box[1] = box[1] / ((640-delta_h) / img.shape[0])
-        box[3] = box[3] / ((640-delta_h) / img.shape[0])
-
-        xmax = box[0] + box[2]
-        ymax = box[1] + box[3]
-        img = cv2.rectangle(img, (int(box[0]), int(box[1])), (int(xmax), int(ymax)), (0, 255, 0), 3)
-        img = cv2.rectangle(img, (int(box[0]), int(box[1]) - 20), (int(xmax), int(box[1])), (0, 255, 0), cv2.FILLED)
-        img = cv2.putText(img, str(classId) + " " + "{:.2f}".format(confidence),
-                          (int(box[0]), int(box[1]) - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
-
-    return img
-
-
 def main():
-    MODEL_PATH = "../model/yolov5s_openvino_model/yolov5s.xml"
+    #                        yolov5s_openvino_model_quantization
+    MODEL_PATH = "../weights/yolov5s_openvino_model/yolov5s.xml"
     IMAGE_PATH = "../imgs/bus.jpg"
+    YAML_PATH  = "../weights/yolov5s_openvino_model/yolov5s.yaml"
 
     # 获取图片,扩展的宽高
     img, input_tensor, delta_w ,delta_h = get_image(IMAGE_PATH)
@@ -178,15 +95,18 @@ def main():
     # 获取模型
     infer_request = get_model(MODEL_PATH, 'CPU')
 
-    start = time.time()
+    # 获取label
+    index2label = get_index2label(YAML_PATH)
 
+    start = time.time()
+    # 设置输入
     infer_request.infer({0: input_tensor})
-    # Step 7. Retrieve inference results
+    # 获取输出
     output = infer_request.get_output_tensor()
     detections = output.data[0]
 
     # Step 8. Postprocessing including NMS
-    img = post(detections, delta_w ,delta_h, img)
+    img = post(detections, delta_w ,delta_h, img, CONFIDENCE_THRESHOLD, SCORE_THRESHOLD, NMS_THRESHOLD, index2label)
     end = time.time()
     print((end - start) * 1000)
 
